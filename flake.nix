12a4e5d84192a4bd55300e57c5ff3a95552ea49b
SkettiSouls <skettisouls@gmail.com>
2024-10-10 02:37:48 -0500

    Fix missing deps
    `nativeBuildInputs` doesn't affect shell scripts, since theirs nothing to build, so now packages are wrapped with their dependencies.

--- flake.nix
+++ flake.nix
@@ -11,6 +11,7 @@
   {
     packages = eachSystem (system:
     let
+      inherit (nixpkgs.lib) mkIf escapeShellArgs;
       pkgs = import nixpkgs { inherit system; };
 
       mkScript = name: deps:
@@ -18,15 +19,23 @@
           name = name;
           nobuild = true;
           src = ./bash;
-          nativeBuildInputs = deps;
-          installPhase = ''
+          nativeBuildInputs = if deps != [] then [ pkgs.makeWrapper ] else [];
+          installPhase = let
+            depPaths = map (p: ["--prefix" "PATH" ":" "${p}/bin"]) deps;
+          in ''
             mkdir -p $out/bin
             cp $src/${name} $out/bin/${name}
-          '';
+          '' + (if deps != [] then ''
+            for file in $out/bin/*; do
+              wrapProgram \
+                $file \
+                ${escapeShellArgs (nixpkgs.lib.flatten depPaths)}
+            done
+          '' else "");
         };
     in
     {
-      chp = mkScript "chp" [];
+      chp = mkScript "chp" [ pkgs.bluez ];
       eat = mkScript "eat" [];
       grime = with pkgs; mkScript "grime" [ libnotify grim slurp ];
       play = mkScript "play" [ pkgs.mpv pkgs.fzf ];
