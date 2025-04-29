091d99e264ecc200b5bb185062b1856253e306f5
SkettiSouls <skettisouls@gmail.com>
2025-04-29 07:22:06 -0500

    Nix: Switch to flake-parts and update
    Also added a link to the original for eat

--- flake.nix
+++ flake.nix
@@ -1,41 +1,40 @@
 {
   inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    systems.url = "github:/nix-systems/default";
   };
 
-  outputs = inputs: with inputs;
-  let
-    eachSystem = nixpkgs.lib.genAttrs (import systems);
-  in
+  outputs = inputs @ { flake-parts, ... }:
+  flake-parts.lib.mkFlake { inherit inputs; }
   {
-    packages = eachSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
+    systems = [ "x86_64-linux" "aarch64-linux" ];
+    perSystem = { pkgs, ... }: {
+      packages = let
+        scripts = with builtins; mapAttrs
+          (script: _: pkgs.stdenvNoCC.mkDerivation {
+            name = script;
+            nobuild = true;
+            src = ./bash;
+            installPhase = ''
+              mkdir -p $out/bin
+              cp $src/${script} $out/bin/${script}
+            '';
+          })
+        (readDir ./bash);
 
-      scripts = with builtins; mapAttrs (script: _: pkgs.stdenvNoCC.mkDerivation {
-        name = script;
-        nobuild = true;
-        src = ./bash;
-        installPhase = ''
-          mkdir -p $out/bin
-          cp $src/${script} $out/bin/${script}
-        '';
-      }) (readDir ./bash);
-
-      mkScript = name: deps:
-        pkgs.writeShellApplication {
-          name = name;
-          runtimeInputs = deps;
-          text = ''
-            ${scripts.${name}}/bin/${name} "$@"
-          '';
-        };
-    in {
-      chp = mkScript "chp" [ pkgs.bluez ];
-      eat = mkScript "eat" [];
-      grime = with pkgs; mkScript "grime" [ libnotify grim slurp ];
-      play = mkScript "play" [ pkgs.mpv pkgs.fzf ];
-    });
+        mkScript = name: runtimeInputs:
+          pkgs.writeShellApplication {
+            inherit name runtimeInputs;
+            text = ''
+              ${scripts.${name}}/bin/${name} "$@"
+            '';
+          };
+      in {
+        chp = mkScript "chp" [ pkgs.bluez ];
+        eat = mkScript "eat" [];
+        grime = with pkgs; mkScript "grime" [ libnotify grim slurp ];
+        play = mkScript "play" [ pkgs.mpv pkgs.fzf ];
+      };
+    };
   };
 }
